1c869ec523be33e249e61d6bc5eae4af43829b70
davq23 <davidquinterogranadillo@gmail.com>
2023-04-16 10:47:48 -0400

    Redis db num

--- db/redis.py
+++ db/redis.py
@@ -4,7 +4,7 @@ from redis import Redis
 redis_client = Redis(
     host=app_config['REDIS_HOST'],
     port=app_config['REDIS_PORT'],
-    db=0,
+    db=app_config['REDIS_DB_NUM'],
     password=app_config['REDIS_PASSWORD'],
     decode_responses=True
 )
