f414c18af6e196e984b91dc2c899372a3b616cda
davq23 <davidquinterogranadillo@gmail.com>
2023-04-23 00:17:15 -0400

    print app secret

--- routers/ledger.py
+++ routers/ledger.py
@@ -38,6 +38,7 @@ async def authorization(request: Request, call_next):
     if prefix != 'Bearer':
         return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={'msg': 'Forbidden'})
 
+    print(app_config['SECRET_KEY'])
     try:
         payload = jwt.decode(bearer, app_config['SECRET_KEY'], ['HS256'])
     except Exception as e:
